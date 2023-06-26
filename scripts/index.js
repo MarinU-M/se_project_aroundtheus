1359e9768e524aa3c249f8266f4501dffad395b8
Marin M-U <marin.umegane.m@gmail.com>
2023-06-26 15:46:34 -0400

    feat: create dynamic card rendering

--- scripts/index.js
+++ scripts/index.js
@@ -25,32 +25,60 @@ const initialCards = [
   },
 ];
 
+// Elements
 const profileEditBtn = document.querySelector("#profile__edit-button");
-const profileEditPopup = document.querySelector("#popup");
+const profileEditmodal = document.querySelector("#modal");
 const profileEditCloseBtn = document.querySelector("#close");
 const profileName = document.querySelector("#profile__name");
 const profileDescription = document.querySelector("#profile__description");
 const profileNameInput = document.querySelector("#name-input");
 const profileDescriptionInput = document.querySelector("#description-input");
-const profileEditForm = profileEditPopup.querySelector("#popup__form");
+const profileEditForm = profileEditmodal.querySelector("#modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+const cardList = document.querySelector("#gallery__cards");
 
-function togglePopup() {
-  profileEditPopup.classList.toggle("popup__opened");
+// Functions
+function togglemodal() {
+  profileEditmodal.classList.toggle("modal__opened");
 }
 
+function getCardElement(cardData) {
+  // clone the template element with all its content and store it in a cardElement variable
+  const cardElement = cardTemplate.cloneNode(true);
+
+  // access the card title and image and store them in variables
+  const cardImageElement = cardElement.querySelector("#card__image");
+  const cardTitleElement = cardElement.querySelector("#card__title");
+  // set the path to the image to the link field of the object
+  cardImageElement.src = cardData.link;
+  // set the image alt text to the name field of the object
+  cardImageElement.alt = cardData.name;
+  // set the card title to the name field of the object, too
+  cardTitleElement.innerText = cardData.name;
+  // return the ready HTML element with the filled-in data
+  return cardElement;
+}
+
+// Event Listner
 profileEditBtn.addEventListener("click", function () {
   profileNameInput.value = profileName.innerText;
-  togglePopup();
+  togglemodal();
 });
 
 profileEditCloseBtn.addEventListener("click", function () {
   profileDescriptionInput.value = profileDescription.innerText;
-  togglePopup();
+  togglemodal();
 });
 
 profileEditForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
   profileName.innerText = profileNameInput.value;
   profileDescription.innerText = profileDescriptionInput.value;
-  togglePopup();
+  togglemodal();
+});
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardList.append(cardElement);
 });
