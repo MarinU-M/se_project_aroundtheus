743aba4fea142ec90eb0e95882de10af56d97c38
Marin M-U <marin.umegane.m@gmail.com>
2023-07-17 13:46:10 -0400

    feat: create disabled save button class

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,14 +2,31 @@
 // pass all the settings on call
 
 const enableValidation = (options) => {
-  console.log(options);
+  const formElements = Array.from(
+    document.querySelectorAll(options.formSelector)
+  );
+  formElements.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+    // look all inputs inside of the forms
+    // loop through all the inputs to see if all are valid
+    //   if inputs are not valid
+    //     get validation message
+    //     add error class to input
+    //     display error message
+    //     disable button
+    //   if inputs are valid
+    //     enable button
+    //     reset error message
+  });
 };
 
 const config = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
+  submitButtonSelector: ".popup__save",
+  inactiveButtonClass: "popup__save_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
 };
